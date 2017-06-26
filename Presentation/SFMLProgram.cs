d9695332fed1384b68e1b6ca82a48b3334366e32
binarygondola <grzesiuczesiu1@wp.pl>
2017-06-26 22:52:31 +0200

    Initial commit
    http://gameprogrammingpatterns.com/game-loop.html

---
+++ Presentation/SFMLProgram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation
+{
+    class SFMLProgram
+    {
+        int xPocisk = 0;
+
+        public SFMLProgram()
+        {
+
+        }
+
+        public void ProcessInput()
+        {
+
+        }
+
+        public void Update()
+        {
+            xPocisk++;
+        }
+
+        public void Render()
+        {
+
+        }
+    }
+}
