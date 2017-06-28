3b4f06965265aa17394c605da3d4770b8e003266
Grzesiek <grzesiuczesiu1@wp.pl>
2017-06-28 16:11:00 +0200

    Add files via upload

---
+++ MyProgram.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.Window;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Presentation
+{
+    class MyProgram : SFMLProgram
+    {
+        RenderWindow window;
+        private bool done;
+
+        public MyProgram()
+        {
+            Console.WriteLine("My");
+        }
+
+        public void Start()
+        {
+            Initialize();
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            double fps = 60;
+            double msForFrame = 1000 / fps;
+
+            double lag = 0;
+            double dt = 0.001;
+
+            double currentTime = watch.ElapsedMilliseconds;
+
+            double AllTime = 0;
+            double realTime = 0;
+            double frameTime = 0;
+
+            int sleepTime = 0;
+            int wtf = 0;
+
+            while (!done)
+            {
+                window.DispatchEvents();
+
+                //real time
+                realTime = watch.ElapsedMilliseconds;
+
+                frameTime = realTime - currentTime;
+                currentTime = realTime;
+                lag += frameTime;
+
+                while (lag > dt)
+                {
+                    Update(dt);
+                    lag -= dt;
+                }
+
+                window.Clear();
+                Render(window);
+                window.Display();
+                AllTime += frameTime;
+
+                sleepTime = (int)((long)AllTime - watch.ElapsedMilliseconds);
+                if (sleepTime < 0)
+                {
+                    sleepTime = 0;
+                    wtf++;
+                }
+
+                Thread.Sleep(sleepTime);
+                //Console.WriteLine(AllTime - watch.ElapsedMilliseconds + " " + AllTime.ToString("000") + " " + watch.ElapsedMilliseconds.ToString("000") + " " + lag.ToString(".000000000"));
+            }
+            Console.Clear();
+            Console.WriteLine("WTF: " + wtf);
+            Console.ReadKey();
+        }
+
+        public void Initialize()
+        {
+            window = new RenderWindow(new VideoMode(500, 600), "Okno", Styles.Close, new ContextSettings() { DepthBits = 24, AntialiasingLevel = 4 });
+            done = false;
+
+            window.Closed += OnClosed;
+            window.KeyPressed += OnKeyPressed;
+        }
+
+        private void OnKeyPressed(object sender, KeyEventArgs e)
+        {
+            switch(e.Code)
+            {
+                case Keyboard.Key.Escape:
+                    done = true;
+                    break;
+                case Keyboard.Key.E:
+                    OnEClicked();
+                    break;
+                case Keyboard.Key.Q:
+                    OnQClicked();
+                    break;
+            }
+        }
+
+        private void OnClosed(object sender, EventArgs e)
+        {
+            done = true;
+        }
+    }
+}
---
+++ SFMLProgram.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using System.Threading;
+
+namespace Presentation
+{
+    class SFMLProgram
+    {
+        double xPocisk = 0;
+        double vPocisku = 100;
+        List<Drawable> toDraw;
+
+        public SFMLProgram()
+        {
+            toDraw = new List<Drawable>();
+            toDraw.Add(new Circle(20, 10, 10));
+        }
+
+        public void ProcessInput()
+        {
+
+        }
+
+        public void Update(double dt)
+        {
+            xPocisk += (dt / 1000) * vPocisku;
+            ((Circle)toDraw[0]).Position = new SFML.System.Vector2f((float)xPocisk, 0);
+            if (xPocisk >= 500)
+            {
+                xPocisk = 0;
+            }
+            if (xPocisk < 0)
+            {
+                xPocisk = 500;
+            }
+        }
+
+        public void Render(RenderWindow window)
+        {
+            for (int i = 0; i < toDraw.Count; i++)
+            {
+                window.Draw(toDraw[i]);
+            }
+        }
+
+        public void OnEClicked()
+        {
+            vPocisku += 200;
+            ((Circle)toDraw[0]).OnEClicked();
+        }
+
+        public void OnQClicked()
+        {
+            vPocisku -= 200;
+            ((Circle)toDraw[0]).OnQClicked();
+        }
+    }
+}
